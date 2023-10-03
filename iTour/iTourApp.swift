4c0ac5edd7dac9acaeeda01bf4720fb7a3dcde23
Boris Romao Antunes <romaoboris@gmail.com>
2023-10-03 20:10:21 -0300

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Boris Romão Antunes on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
