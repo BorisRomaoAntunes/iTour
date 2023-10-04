211f7bb5a311c9754c0a285d98d6ca6944223ab4
Boris Romao Antunes <romaoboris@gmail.com>
2023-10-03 21:01:33 -0300

    Implementation home list and structure for SwiftData

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -4,18 +4,33 @@
 //
 //  Created by Boris Romão Antunes on 03/10/23.
 //
-
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Query var destinantions: [Destination]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                ForEach(destinantions){ destinantion in
+                    VStack(alignment: .leading){
+                        Text(destinantion.name)
+                            .font(.headline)
+                        Text(destinantion.date.formatted(date: .long, time: .standard))
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add Amostra", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples(){
+        let rome = Destination(name: "Romar")
+        let brazil = Destination(name: "Brasil")
+        let japao = Destination(name: "Japão")
     }
 }
 
